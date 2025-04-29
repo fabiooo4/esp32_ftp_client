6e5add7c651a68b6effe1cb377fea7c4854e2d7f
fabiooo4 <fabioirimie@gmail.com>
2025-04-29 10:05:14 +0200

    fixed menuconfig and added FTP connection

--- main/ftp.c
+++ main/ftp.c
@@ -1,6 +1,7 @@
 #include "esp_bit_defs.h"
 #include "esp_err.h"
 #include "esp_log.h"
+#include "ftplib.h"
 
 // Menuconfig ----------------------------
 #define FTP_SERVER_IP CONFIG_FTP_SERVER_IP
@@ -15,6 +16,31 @@
 static const char *FTP_TAG = "FTP";
 
 esp_err_t connect_ftp_server(void) {
-  ESP_LOGE(FTP_TAG, "Todo: Implement FTP client");
-  return FTP_FAILURE;
+  // Open FTP server
+  ESP_LOGI(FTP_TAG, "FTP Server IP: %s", FTP_SERVER_IP);
+  ESP_LOGI(FTP_TAG, "FTP User     : %s", FTP_USER);
+
+  static NetBuf_t *ftpClientNetBuf = NULL;
+  FtpClient *ftpClient = getFtpClient();
+
+  int connect =
+      ftpClient->FtpConnect(FTP_SERVER_IP, FTP_SERVER_PORT, &ftpClientNetBuf);
+
+  ESP_LOGI(FTP_TAG, "connect=%d", connect);
+  if (connect == 0) {
+    ESP_LOGE(FTP_TAG, "FTP server connect fail");
+    return FTP_FAILURE;
+  }
+
+  // Login FTP server
+  int login = ftpClient->FtpLogin(FTP_USER, FTP_PASSWORD, ftpClientNetBuf);
+
+  ESP_LOGI(FTP_TAG, "login=%d", login);
+
+  if (login == 0) {
+    ESP_LOGE(FTP_TAG, "FTP server login fail");
+    return FTP_FAILURE;
+  }
+
+  return FTP_SUCCESS;
 }
